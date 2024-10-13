3d7694206fb7bbf4400ac0bf7895ae4e8e40a8a2
Jaime de Greiff <alegreiff@gmail.com>
2024-10-13 17:51:48 -0500

    casi closed

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useMemo, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { useStatistics } from "./useStatistics";
 import { Chart } from "./Chart";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const staticData = useStaticData();
+
   const statistics = useStatistics(10);
   const [activeView, setActiveView] = useState<View>("CPU");
   const cpuUsages = useMemo(
@@ -39,8 +39,8 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <header className="absolute top-0 left-0 w-full p-4 bg-slate-700 flex [-webkit-app-region:drag]">
+    <div className="container mx-auto">
+      <header className=" h-12 w-full p-4 bg-slate-700 flex [-webkit-app-region:drag]">
         <button
           onClick={() => window.electron.sendFrameAction("CLOSE")}
           className="[-webkit-app-region:no-drag] rounded-full w-4 p-2 h-4 m-1 bg-red-500"
@@ -57,30 +57,71 @@ function App() {
           id="maximize"
         ></button>
       </header>
-      <div>
-        <a href="https://youtu.be/fP-371MN0Ck?t=5902">Video tutorial</a>
-        <div className="h-48">
-          <Chart data={activeUsages} maxDataPoints={10} />
+      <div className="flex flex-row gap-8 justify-between p-8">
+        <div className="w-1/2">
+          <SelectOption
+            onClick={() => setActiveView("CPU")}
+            title="CPU"
+            view="CPU"
+            subTitle={staticData?.cpuModel ?? ""}
+            data={cpuUsages}
+          />
+          <SelectOption
+            onClick={() => setActiveView("RAM")}
+            title="RAM"
+            view="RAM"
+            subTitle={(staticData?.totalMemoryGB.toString() ?? "") + " GB"}
+            data={ramUsages}
+          />
+          <SelectOption
+            onClick={() => setActiveView("STORAGE")}
+            title="STORAGE"
+            view="STORAGE"
+            subTitle={(staticData?.totalStorage.toString() ?? "") + " GB"}
+            data={storageUsages}
+          />
         </div>
-
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="h-48 w-1/2 flex self-center">
+          <Chart
+            selectedView={activeView}
+            data={activeUsages}
+            maxDataPoints={10}
+          />
+        </div>
       </div>
-      <h1> PBS:: Vite + React</h1>
-      <div className="card">
-        <button className="" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
+}
+
+function SelectOption(props: {
+  title: string;
+  view: View;
+  subTitle: string;
+  data: number[];
+  onClick: () => void;
+}) {
+  return (
+    <button onClick={props.onClick} className="block w-full hover:bg-stone-700">
+      <div className="flex gap-8 cursor-pointer">
+        <div>{props.title}</div>
+        <div>{props.subTitle}</div>
+      </div>
+      <div className="w-full h-16 ">
+        <Chart selectedView={props.view} data={props.data} maxDataPoints={10} />
+      </div>
+    </button>
+  );
+}
+function useStaticData() {
+  const [staticData, setStaticData] = useState<StaticData | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      setStaticData(await window.electron.getStaticData());
+    })();
+  }, []);
+
+  return staticData;
 }
 
 export default App;
--- src/ui/BaseChart.tsx
+++ src/ui/BaseChart.tsx
@@ -9,21 +9,19 @@ import {
 
 type BaseChartProps = {
   data: { value: number | undefined }[];
-  /* fill: string;
-    stroke: string; */
+  fill: string;
+  stroke: string;
 };
 
 export function BaseChart(props: BaseChartProps) {
   return (
     <ResponsiveContainer width={"100%"} height={"100%"}>
       <AreaChart data={props.data}>
-        <CartesianGrid stroke="gray" strokeDasharray="5 5" fill="#1C1C1C" />
+        <CartesianGrid stroke="#333" strokeDasharray="5 5" fill="#1C1C1C" />
         <Area
           fillOpacity={0.3}
-          /* fill={props.fill}
-                    stroke={props.stroke} */
-          fill={"#0a4d5c"}
-          stroke={"#5dd4ee"}
+          fill={props.fill}
+          stroke={props.stroke}
           strokeWidth={3}
           type="monotone"
           dataKey="value"
